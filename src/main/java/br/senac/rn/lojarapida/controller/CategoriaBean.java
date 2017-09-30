885d6af09d8ce1caec8695dd3b98310602b76624
alexwallax <31941706+alexwallax@users.noreply.github.com>
2017-09-29 21:18:30 -0300

    lojarapida criada

--- src/main/java/br/senac/rn/lojarapida/controller/CategoriaBean.java
+++ src/main/java/br/senac/rn/lojarapida/controller/CategoriaBean.java
@@ -2,6 +2,7 @@ package br.senac.rn.lojarapida.controller;
 
 import br.senac.rn.lojarapida.dao.CategoriaDAO;
 import br.senac.rn.lojarapida.model.Categoria;
+import java.util.List;
 import javax.faces.bean.ManagedBean;
 
 @ManagedBean(name = "categoriaBean") // informa p o jsf e um gerenciador de telas
@@ -15,9 +16,10 @@ public class CategoriaBean {
         this.categoria = new Categoria();
     }
     
-    
-    
-    
+    public List<Categoria> getCategorias(){
+       CategoriaDAO dao = new CategoriaDAO();
+       return dao.selectAll();
+    }   
 
     public Categoria getCategoria() {
         return categoria;
